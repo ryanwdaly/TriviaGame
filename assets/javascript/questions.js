211885bc05e91ca6cf095a5461e407b629c65af3
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-05-11 21:22:02 -0400

    finished

---
+++ assets/javascript/questions.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        "question": "What year did Apollo 11 land on the moon?", 
+        "choice": ["1968", "1969", "1970", "1971"],
+        "answer": 1,
+     },
+     {
+        "question": "Who was the first person in space?", 
+        "choice": ["Yuri Gargarin", "Neil Armstrong", "Pepe Silvia", "Alan Shepard"],
+        "answer": 0,
+     }, 
+     {
+         "question": "What was the center named in 1962 when first designated an independant NASA installation?", 
+        "choice": ["Florida Launch Center", "Cape Kennedy", "Launch Operations Center", "Merrit Island" ],
+        "answer": 2,
+    }, 
+    {
+        "question": "Who was the first American to orbit earth?", 
+        "choice": ["Ben Affleck", "Scott Carpenter", "John Glenn", "Alan Shepard" ],
+        "answer": 2,
+    }, 
+    {
+        "question": "What was the name of the first Space Shuttle to launch?", 
+        "choice": ["Discovery", "Challenger", "Enterprise", "Columbia" ],
+        "answer": 3,
+    }, 
+    {
+        "question": "What Caused teh Apollo 13 Mission to be aborted?", 
+        "choice": ["Misaigned Trajectory", "Oxygen Rupture", "Sickness", "Propulsion system ran out of fuel" ],
+        "answer": 1,
+    }, 
+    {
+        "question": "When did Skylab reenter earth's atmosphere?", 
+        "choice": ["1978", "1979", "1980", "1981" ],
+        "answer": 1,
+    }, 
+    {
+        "question": "Who was the first civilian to fly in space?", 
+        "choice": ["Neil Armstrong", "Sally Ride", "John Glenn", "John Young" ],
+        "answer": 0,
+    }
+]
+
+    
